25ad7e8dce54621a87efae6a08c2ff65499a1153
Travis Douce <travisdouce@gmail.com>
2012-02-20 10:47:08 -0500

    Add comment

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,6 +3,8 @@ class Visit < ActiveRecord::Base
   validates :dive_start,            :presence => true
   validates :dive_end,              :presence => true
   validates :sample_start,          :presence => true
+
+  # Custom validations
   validates_is_after :dive_end,     :after => :dive_start
   validates_is_after :sample_end,   :after => :sample_start
   validates_is_after :sample_start, :after => :dive_start
