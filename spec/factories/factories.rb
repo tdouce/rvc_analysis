12af4840bf1bb98123a208c25cca587b65f2bd4c
Travis Douce <travisdouce@gmail.com>
2012-02-21 17:46:09 -0500

    Updates factories#rb

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -9,6 +9,10 @@ end
 
 Factory.define :visit do |f|
   current_time  = Time.now
-  f.dive_start  current_time
-  f.dive_end    current_time + 1.hour
+  f.dive_start          current_time
+  f.sample_start        current_time + 30.minutes
+  f.sample_end          current_time + 35.minutes
+  f.dive_end            current_time + 1.hour
+  f.substrate_max_depth 50 
+  f.substrate_min_depth 30 
 end
