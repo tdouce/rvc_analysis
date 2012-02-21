2a57028cd6c4cdf27a061666dc5ba4e77b20ba62
Travis Douce <travisdouce@gmail.com>
2012-02-21 13:56:01 -0500

    Adds presence validation for diver_visits on model#visits#rb

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -8,6 +8,7 @@ class Visit < ActiveRecord::Base
   validates :dive_start,            :presence => true
   validates :dive_end,              :presence => true
   validates :sample_start,          :presence => true
+  validates :diver, :presence => true
 
   # Custom validations
   validates_is_after :dive_end,     :after => :dive_start
