db52cc10843aa46e8c631bec73742f7ead3ac8ba
Travis Douce <travisdouce@gmail.com>
2012-02-23 13:20:15 -0500

    Creates a method to display both scientific_name and common_name

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,4 +4,8 @@ class Animal < ActiveRecord::Base
 
   validates :scientific_name, :presence => true, :uniqueness => true
   validates :common_name,     :presence => true
+
+  def combined_name
+    [self.scientific_name, self.common_name].join(" - ")
+  end
 end
