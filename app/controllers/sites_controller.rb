cdc480eb1b04990a92a2c4c85850a847a64824bb
Travis Douce <travisdouce@gmail.com>
2012-02-22 19:43:26 -0500

    Adds edit and update action

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -15,9 +15,18 @@ class SitesController < ApplicationController
   end
 
   def edit
+    @site = Site.find(params[:id])
   end
 
   def update
+    @site = Site.find(params[:id])
+    if @site.update_attributes(params[:site])
+      flash[:success] = "Site was updated"
+      redirect_to sites_url
+    else
+      flash[:failure] = "Site was NOT updated"
+      render 'edit'
+    end
   end
 
   def index
